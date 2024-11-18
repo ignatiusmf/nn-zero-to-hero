7ff5755e3674600bd364a80bde3c0dd2407aef80
Ignatius <ignatio.ferreira@gmail.com>
2024-11-18 22:42:01 +0200

    idk

--- makemore.py
+++ makemore.py
@@ -8,102 +8,20 @@ import torch.nn.functional as F
 g = torch.Generator().manual_seed(2)
 
 
-def hypothesis1():
-# NOTE: HYPOTHESIS: P1[:, 1] = P2[1]. 
-# HYPOTHESIS: Plausible. For some reason werk dit nie uit nie. Maar N1[:, 1] = N2[1]
-
-    P1, N1 = create_trigram_probability_distribution()
-    P2, N2 = create_bigram_probability_distribution()
-
-    test = torch.zeros((27), dtype=torch.float)
-    for i in range(27):
-        test += P1[i,1]
-    test = test.float() / test.float().sum()
-    print(test)
-
-    print()
-
-    counts = torch.zeros((27), dtype=torch.int32)
-    for i in range(27):
-        counts += N1[i, 1]
-    pprint(N2[1])
-    pprint(counts)
-
-    count_prob = counts.float() / counts.float().sum()
-    pprint(count_prob.sum())
-
-    pprint("\n retry")
-
-    retry = torch.zeros((27), dtype=torch.float)
-    for i in range(27):
-        retry += P1[:, 1, i]
-    print(retry.float().sum())
-    pprint(retry)
-
-
-def hypothesis2():
-# NOTE: HYPOTHESIS: Die manually created probability distribution moet dieselfde wees as n synthetic een wat gemaak is deur n large sample van woorde generate deur die eerste distriubtion. 
-# NOTE: HYPOTHESIS: Confirmed, die synthetic een approach die ground truth een
-    names = generate_name(100000)
-    P1, N1 = create_bigram_probability_distribution()
-    CP1, CN1 = create_bigram_probability_distribution(names)
-
-    print(P1[0])
-    print('\n')
-    print(CP1[0])
-
-    compare_distributions(CP1, P1)
-
-
-def hypothesis3():
-# NOTE: Hypothesis: TRIGRAM/BIGRAM probability distrubtion wat generated is van TRIGRAM/BIGRAM generated words moet moet die TRIGRAM/BIGRAM probability distibution generated van die words dataset approach.
-# NOTE: Hypothesis: Confirmed 
-    tri_names = generate_name_trigram(100000)
-    bi_names = generate_name(100000)
-
-
-    distros = {
-        "B_GT": create_bigram_probability_distribution(),
-        "B_B": create_bigram_probability_distribution(bi_names),
-        "B_T": create_bigram_probability_distribution(tri_names),
-        "T_GT": create_trigram_probability_distribution(),
-        "T_B": create_trigram_probability_distribution(bi_names),
-        "T_T": create_trigram_probability_distribution(tri_names) 
-    }
-
-    for k,v in distros.items():
-        print('\n', k)
-        for k1,v1 in distros.items():
-            if k == k1:
-                print('skipping', k, k1)
-                continue
-
-            print(k,k1)
-            if k[0] == "B" and k1[0] == "T":
-                p1, p2 = v[0], v1[1]
-            elif k[0] == "T" and k1[0] == "B":
-                p1, p2 = v[1], v1[0]
-            elif k[0] == "T" and k1[0] == "T":
-                p1, p2 = v[1], v1[1]
-            else:
-                p1,p2 = v[0], v1[0]
-
-            compare_distributions(p1, p2)
-
-
-
-def flatten_tensor(counts): # TODO: THIS SHIT AINT WORKING. HOEKOM KAN EK NIE DIE PROBABILITIES VAN MY TRIGRAM TENSOR TRANSFORM IN DIESELFDES VAN MY BIGRAM TENSOR??? MAAR COUNTS WERK!?
-    flattened_counts = torch.zeros((27,27), dtype=torch.int32)
-    for i in range(27):
-        for j in range(27):
-            flattened_counts[i] += counts[j, i]
-
-    flattened_probs = torch.zeros((27,27), dtype=torch.float)
-    for i in range(27):
-        flattened_probs[i] = flattened_counts[i].float() / flattened_counts[i].float().sum()
-    return flattened_probs, flattened_counts
-
 def compare_distributions(dist1, dist2):
+    def flatten_tensor(counts): # TODO: THIS SHIT AINT WORKING. HOEKOM KAN EK NIE DIE PROBABILITIES VAN MY TRIGRAM TENSOR TRANSFORM IN DIESELFDES VAN MY BIGRAM TENSOR??? MAAR COUNTS WERK!?
+        flattened_counts = torch.zeros((27,27), dtype=torch.int32)
+        for i in range(27):
+            for j in range(27):
+                flattened_counts[i] += counts[j, i]
+
+        flattened_probs = torch.zeros((27,27), dtype=torch.float)
+        for i in range(27):
+            flattened_probs[i] = flattened_counts[i].float() / flattened_counts[i].float().sum()
+        return flattened_probs, flattened_counts
+    
+    
+    
     if len(dist1.shape) == 3 and len(dist2.shape) == 2:
         dist1,counts = flatten_tensor(dist1)
     elif len(dist2.shape) == 3 and len(dist1.shape) == 2:
