94f66fc07037b9ec1e2b4d4c33c0ba2405b4bf33
Ignatius <ignatio.ferreira@gmail.com>
2024-12-13 20:56:37 +0200

    yeet

--- makemore/4_exercise.py
+++ makemore/4_exercise.py
@@ -1,33 +1,34 @@
 import os
 import math
-import time
 import numpy as np
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-from pprint import pprint
 import random
 
-### Configuration ###
+# Configuration ###
 random.seed(0)
 torch.manual_seed(0)
 torch.set_default_device("cuda")
 
-### Loading data ###
-words = open(os.path.dirname(os.path.abspath(__file__)) + '/names.txt', 'r').read().splitlines()
+# Loading data ###
+words = open(os.path.dirname(os.path.abspath(__file__)) +
+             '/names.txt', 'r').read().splitlines()
 chars = sorted(set(''.join(words)))
 
-stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi = {s: i+1 for i, s in enumerate(chars)}
 stoi['.'] = 0
-itos = {i:s for s,i in stoi.items()}
+itos = {i: s for s, i in stoi.items()}
 
 
-### Creating datasets ###
-block_size = 5 
+# Creating datasets
+block_size = 5
+
+
 def build_dataset(words):
     X, Y = [], []
     for w in words:
-        context = [0] * block_size 
+        context = [0] * block_size
         for c in w + '.':
             X.append(context)
             Y.append(stoi[c])
@@ -38,6 +39,7 @@ def build_dataset(words):
     Y = torch.tensor(Y)
     return X, Y
 
+
 n1 = int(0.8*len(words))
 n2 = int(0.9*len(words))
 random.shuffle(words)
@@ -45,20 +47,21 @@ Xtr, Ytr = build_dataset(words[:n1])
 Xde, Yde = build_dataset(words[n1:n2])
 Xte, Yte = build_dataset(words[n2:])
 
-embed_size = 3 
+embed_size = 3
 vocab_size = len(chars) + 1
 n_hidden = 200
 
 C = torch.randn((vocab_size, embed_size))
-W = torch.randn((embed_size*block_size, n_hidden)) * 5/3/(embed_size*block_size)**0.5
+W = torch.randn((embed_size*block_size, n_hidden)) * \
+    5/3/(embed_size*block_size)**0.5
 O = torch.randn((n_hidden, vocab_size)) * 0.01
-o = torch.zeros((vocab_size)) 
+o = torch.zeros((vocab_size))
 
-bn_scale = torch.ones((1,n_hidden))
-bn_shift= torch.zeros((1,n_hidden))
+bn_scale = torch.ones((1, n_hidden))
+bn_shift = torch.zeros((1, n_hidden))
 
-bnstd_running = torch.ones((1,n_hidden))
-bnmean_running = torch.zeros((1,n_hidden))
+bnstd_running = torch.ones((1, n_hidden))
+bnmean_running = torch.zeros((1, n_hidden))
 
 parameters = [C, W, bn_shift, bn_scale, O, o]
 for p in parameters:
@@ -67,7 +70,7 @@ for p in parameters:
 batch_size = 32
 
 epochs = 10000
-lr = 0.1 
+lr = 0.1
 losses = []
 for epoch in range(epochs):
 
@@ -75,10 +78,10 @@ for epoch in range(epochs):
 
     emb = C[Xtr[batch]].view(-1, block_size*embed_size)
 
-    hpreact = emb @ W 
+    hpreact = emb @ W
 
-    bnmean = hpreact.mean(0,keepdim=True) 
-    bnstd = hpreact.std(0,keepdim=True)
+    bnmean = hpreact.mean(0, keepdim=True)
+    bnstd = hpreact.std(0, keepdim=True)
     with torch.no_grad():
         bnstd_running = bnstd_running * 0.999 + bnstd * 0.001
         bnmean_running = bnmean_running * 0.999 + bnmean * 0.001
@@ -87,10 +90,8 @@ for epoch in range(epochs):
 
     h = torch.tanh(hpreact)
 
-
     logits = h @ O + o
     loss = F.cross_entropy(logits, Ytr[batch])
-
 
     for p in parameters:
         p.grad = None
@@ -101,23 +102,19 @@ for epoch in range(epochs):
     for p in parameters:
         p.data += -lr * p.grad
 
-
     if epoch == epochs * 0.8:
         lr = 0.01
         batch_size = batch_size * 2
         print("\nAdjusting training\n")
 
-
-    if epoch  % (epochs/10) == 0: 
+    if epoch % (epochs/10) == 0:
         print(epoch, loss.item())
 
 
-
-plt.hist(hpreact.view(-1).tolist(),50)
+plt.hist(hpreact.view(-1).tolist(), 50)
 plt.show()
-plt.hist(h.view(-1).tolist(),50)
+plt.hist(h.view(-1).tolist(), 50)
 plt.show()
-
 
 
 def evaluate():
@@ -129,22 +126,26 @@ def evaluate():
     loss = F.cross_entropy(logits, Yde)
     print(f"\nDev loss - {loss.item()}\n")
 
+
 evaluate()
-if epochs>25:
-    moving_average=int(batch_size*math.log10(epochs)) #int(epochs/10)
-    plt.plot(np.convolve(losses,np.ones(moving_average)/moving_average,mode='valid'))
+if epochs > 25:
+    moving_average = int(batch_size*math.log10(epochs))  # int(epochs/10)
+    plt.plot(np.convolve(losses, np.ones(
+        moving_average)/moving_average, mode='valid'))
 else:
     plt.plot(losses)
 
 plt.show()
 
+
 for i in range(10):
     context = torch.tensor([[0] * block_size])
-    name = [] 
+    name = []
     while True:
         emb = C[context].view(-1, block_size*embed_size)
-        hpreact = emb @ W  
-        bnhpre = bn_scale * (hpreact - bnmean_running) / bnstd_running - bn_shift
+        hpreact = emb @ W
+        bnhpre = bn_scale * (hpreact - bnmean_running) / \
+            bnstd_running - bn_shift
         h = torch.tanh(bnhpre)
         logits = h @ O + o
 
@@ -156,7 +157,6 @@ for i in range(10):
 
         name.append(itos[next])
 
-        context = torch.cat((context[:,1:], torch.tensor([[next]])), dim=1)
+        context = torch.cat((context[:, 1:], torch.tensor([[next]])), dim=1)
 
     print(i, ''.join(name))
-
